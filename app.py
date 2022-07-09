b56df0f83466cf928056233239a3d698c1a8ffc3
Derrick Hoon <derrickhoon@Derricks-Mac-Air-M1.local>
2022-07-10 05:28:55 +0800

    Minor bug fixes
    Signed-off-by: Derrick Hoon <derrickhoon@Derricks-Mac-Air-M1.local>

--- app.py
+++ app.py
@@ -3,9 +3,29 @@ from flask_cors import CORS
 
 from models.CharityActivity import charity_activity_db
 from models.CharityActivityTag import charity_activity_tag_db
+from models.User import user_db
+from models.UserFavourite import user_fav_db
+
+from routes.RecommenderRoutes import RecommenderRoutes
 
 app: Flask = Flask(__name__)
 app.config.from_object("config")
 
+CORS(app)
+
 charity_activity_db.init_app(app)
 charity_activity_tag_db.init_app(app)
+user_db.init_app(app)
+user_fav_db.init_app(app)
+
+with app.app_context():
+    charity_activity_db.create_all()
+    charity_activity_tag_db.create_all()
+    user_db.create_all()
+    user_fav_db.create_all()
+
+app.register_blueprint(RecommenderRoutes, url_prefix = "")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
--- routes/RecommenderRoutes.py
+++ routes/RecommenderRoutes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
+
 from controllers.Recommender import Recommender
 
 RecommenderRoutes = Blueprint("RecommenderRoutes", __name__)
 
-RecommenderRoutes.get("/activities")
-RecommenderRoutes.get("/")
-RecommenderRoutes.get("/activities")
+RecommenderRoutes.get("/activities")(Recommender.get_activities)
+RecommenderRoutes.get("/user/<int:id>/favourites")(Recommender.get_user_fav)
+RecommenderRoutes.get("/user/<int:id>/recommended")(Recommender.get_recommendations)
